fef90cd0396faeb5bf46cef66f72a05ac2662e77
satyam kumar <167162735+satyamkumar1507@users.noreply.github.com>
2026-01-12 11:57:03 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,6 @@
 variable "aws_region" {
   description = "AWS region where resources will be provisioned"
-  default     = "us-east-2"
+  default     = "eu-west-1"
 }
 
 variable "ami_id" {
@@ -10,5 +10,5 @@ variable "ami_id" {
 
 variable "instance_type" {
   description = "Instance type for the EC2 instance"
-  default     = "t2.large"
+  default     = "m7i-flex.large"
 }
